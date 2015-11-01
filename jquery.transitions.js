c2d09389fcfd6decdd9ddff91cc0995ec53c0cdf
Miloš Đakonović <milos@djakonovic.net>
2015-11-01 18:22:45 +0100

    Changes to engine, new features, license & other stuff

--- jquery.transitions.js
+++ jquery.transitions.js
@@ -1,4 +1,14 @@
-(function($){
+/************************************************
+ *                                              *
+ *  jQuery transitions                          *
+ *  @author Milos Djakonovic (@Miloshio)        *
+ *  Copyright 2015 Milos Djakonovic             *
+ *  Licensed under the MIT license              *
+ *                                              *
+ ************************************************/
+
+
+(function(w,d,$){
     
 
     /**
@@ -6,6 +16,12 @@
     *   Part I
     *   cross browser transitionend event
     *   from modernizr & twbootstrap
+    *
+    *   --------
+    *   Bootstrap v3.3.5 (http://getbootstrap.com)
+    *   Copyright 2011-2015 Twitter, Inc.
+    *   Licensed under the MIT license
+    *   --------
     *
     *   TODO: check if $.support.transition.end already exists
     *   (when Bootstrap's code is already included)
@@ -18,7 +34,7 @@
     */
     
     function transitionEnd() {
-        var el = document.createElement('bootstrap');
+        var el = d.createElement('bootstrap');
 
         var transEndEventNames = {
           WebkitTransition : 'webkitTransitionEnd',
@@ -41,7 +57,7 @@
         var $el = this;
         $(this).one('bsTransitionEnd', function () { called = true });
         var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
-        setTimeout(callback, duration);
+        w.setTimeout(callback, duration);
         return this;
     }
   
@@ -49,6 +65,7 @@
 
     if (!$.support.transition) return; // no transitions supported. We're out of here,
     // $.fn.transition will not be declared...
+    // needs to be handled different way
 
     $.event.special.bsTransitionEnd = {
       bindType: $.support.transition.end,
@@ -74,7 +91,7 @@
             })
         },
         pause = function(){
-            var computedStyle = window.getComputedStyle(self[0], null);
+            var computedStyle = w.getComputedStyle(self[0], null);
             for(var property in self.data('tansition_fn_data')['prevFtrProp']){
                 self.css(property, computedStyle[property]).css('transition-duration', '0ms' );
             }
@@ -83,7 +100,7 @@
             
         },
         finish = function(){
-            
+            self.css('transition-duration', '0ms');
         },
         sequence = function(){
             for(var i=0; i<arg2.length;i++){
@@ -93,6 +110,53 @@
             
         composeCubicBezier = function(param){
             return ['cubic-bezier(' ,  param.join(', ') , ')'].join('');
+        },
+        // transition to properties
+        trToProps = {},
+        
+        //nubmer of properties being transitionend
+        //very unoptimized enumeration type since
+        //solve it later
+        numOfProps=0,
+            
+        composeTransitionProperty = function(cssProps,self){
+
+            var transitionStr = '';
+            for(var prop in cssProps){
+                
+                if(typeof(cssProps[prop][2])==='undefined')
+                    var ease = 'ease';
+                else var ease = cssProps[prop][2];
+                if(typeof(cssProps[prop][1])==='undefined')
+                    var duration = '400';
+                else var duration = cssProps[prop][1];
+                
+                (typeof(ease)==='object') && (ease = composeCubicBezier(ease))
+                
+
+                transitionStr += prop + ' ' + duration + 'ms ' + ease + ', ';
+                trToProps[prop] = cssProps[prop][0];
+                numOfProps++;
+                //save state for back function
+                //prevFtrProp previous for transition property
+                (typeof(self.data('tansition_fn_data'))!=='object') && self.data('tansition_fn_data', {prevFtrProp:{}});
+
+                self.data('tansition_fn_data')['prevFtrProp'][prop] = self.css(prop);
+
+            }
+
+            return transitionStr.slice(0, - 2);
+        },
+        exec = function(cssProps,callback,self){
+            //finally set transition property
+            self.css('transition', composeTransitionProperty(cssProps,self) )
+            .css(trToProps).on('bsTransitionEnd', function(){
+                if(--numOfProps === 0){
+                    //only calling transitionend for ONE property - the one with longest duration - lost it for now
+                    if(callback)
+                    callback.call(self,self);
+                }
+            });
         }
         
         
@@ -104,57 +168,12 @@
             (arg1==='resume')    && resume();
             (arg1==='finish')    && finish();
             (arg1==='sequence')  && sequence();
-            
-        } else {
 
+        } else
             //action
-            var transitionStr = '',
-            i=0,
-            // transition to properties    
-            trToProps = {};
-            for(var prop in arg1){
-                
-                if(typeof(arg1[prop][2])==='undefined')
-                    var ease = 'ease';
-                else var ease = arg1[prop][2];
-                if(typeof(arg1[prop][1])==='undefined')
-                    var duration = '400';
-                else var duration = arg1[prop][1];
-                
-                (typeof(ease)==='object') && (ease = composeCubicBezier(ease))
-                
-
-                transitionStr += prop + ' ' + duration + 'ms ' + ease + ', ';
-                trToProps[prop] = arg1[prop][0];
-                i++;
-                //save state for back function
-                //prevFtrProp previous for transition property
-                (typeof(this.data('tansition_fn_data'))!=='object') && this.data('tansition_fn_data', {prevFtrProp:{}});
-
-                this.data('tansition_fn_data')['prevFtrProp'][prop] = this.css(prop);
-
-            }
-
-
-            //finally set transition property
-            this.css('transition', transitionStr.slice(0, - 2))
-            .css(trToProps).on('bsTransitionEnd', function(){
-                if(--i === 0){
-                    //only calling transitionend for ONE property - the one with longest duration
-                    if(arg2)
-                    arg2.call(self,self);
-                }
-            });            
-        }
-        
-        
+            exec(arg1,arg2,self);
 
         return this;
-    }    
-    
-   
-    
-    
-  
-  
-})(jQuery);
+    }
+
+})(window,document,jQuery);
